a97d3ecf806d26f385f15652b2cdb442ac156300
OneRiver98 <167278772+OneRiver98@users.noreply.github.com>
2024-05-30 08:58:38 +0900

    [24.05.30]AddCharacterManagerScripts
    캐릭터매니저 싱글턴 스크립트 추가.

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    private static CharacterManager _instance;
+    public static CharacterManager Instance
+    {
+        get 
+        { 
+            if (_instance == null)
+            { 
+                _instance = new GameObject("CharacterManager").AddComponent<CharacterManager>(); 
+            }
+            return _instance;
+        }
+    }
+
+
+    private static Player _player;
+    public static Player Player
+    {
+        get { return _player; }
+        set { _player = value; }
+    }
+
+
+    //private void Awake()
+    //{
+    //    if (_instance == null) 
+    //    {
+    //        _instance = this;
+    //        DontDestroyOnLoad(gameObject);
+    //    }
+    //    else 
+    //    {
+    //        if (_instance == this)
+    //        { 
+    //            Destroy(gameObject); 
+    //        }
+    //    }
+    //}
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+    [Range(0, 20)] public float speed;
+
+    [Range(0, 100)] public float Health;
+    [Range(0, 100)] public float stamina;
+
+    
+    private void start()
+    {
+
+    }
+}
